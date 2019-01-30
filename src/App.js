4815d43508ecae613d75d6672a864ed5e3e1185a
aishah21 <aishah-almaghrabi@hotmail.com>
2019-01-30 10:09:53 +0300

    user form insert to user by username route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import About from "./components/About";
+import UserForm from "./components/UserForm"
 
 
 
@@ -13,30 +14,186 @@ class App extends Component {
     super();
     this.state = {
       activeNav: "home" ,
-      // userInfo:"",
-      // userForm:false,
-      // loginForm: true
-
-
+      userInfo: undefined,
+      activityInfo: undefined,
+      activityForm:false,
+      // username: '',
+      // loginForm: false,
+      // results: [],
+      userForm: false,
+      // showProfile: false
+ 
     };
   }
 
   onNavClick = activeNav => {
     this.setState({ activeNav });
   };
-  // handleRegister() {
+  // show the login form to show
+  // setLoginForm() {
   //   this.setState({
-  //     userForm: !this.state.userForm,
-  //     loginForm: false
+  //     loginForm: !this.state.loginForm,
+  //     userForm: false
   //   });
   // }
-  // renderUserForm() {
-  //   return <UserForm userInfo={this.state.userInfo} 
-  //                    handleFormSubmit={this.handleFormSubmit.bind(this)} 
-  //                    handleRegister={this.handleRegister.bind(this)} 
-  //                    />
+
+  handleChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  // handle the user name to check if he is registered or no 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    fetch(`${API_URL}user/${this.state.username}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          userInfo: data,
+          loginForm: false
+        })
+      })
+      .catch(error => {
+        console.log('App.js handleSubmit function: ', error);
+        alert({
+          title: "This username NOT Registered",
+          icon: "warning"
+        });
+      })
+    console.log(this.state.userInfo);
+  }
+  //handel thre form sumbmission if the user to create a new user or updtae user info
+  handleFormSubmit(user) {
+    this.state.userInfo ? this.updateUserInfo(user) : this.createNewUser(user)
+  }
+
+  // update user information in the database 
+  updateUserInfo(user) {
+    console.log("##", user);
+    const url = `${API_URL}user/${user.id}`
+
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ 
+          userInfo: data,
+          userForm: false 
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+    createNewUser(user) {
+      const url = `${API_URL}user`
+  console.log("uuuuuuuuuuuser info",user)
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log("uuuuuuuuuuuser data",data)
+
+          this.setState({ 
+            userInfo: data,
+            userForm: false
+           })
+        })
+        .catch(error => {
+          console.log('createNewUser Error: ', error)
+          alert("Tnis username or email is Registerd.");
+        })
+    }
+
+  //handel thre form sumbmission if the user to create a new user or updtae user info
+  handleFormSubmit(user) {
+    this.state.userInfo ? this.updateUserInfo(user) : this.createNewUser(user)
+  }
+
+  // update user information in the database 
+  updateUserInfo(user) {
+    console.log("##", user);
+    const url = `${API_URL}user/${user.id}`
+
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ 
+          userInfo: data,
+          userForm: false 
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+  handleRegister() {
+    this.setState({
+      userForm: !this.state.userForm,
+      loginForm: false
+    });
+  }
+
+  // render user form for the registeration
+  renderUserForm() {
+    return <UserForm userInfo={this.state.userInfo} 
+                     handleFormSubmit={this.handleFormSubmit.bind(this)} 
+                     handleRegister={this.handleRegister.bind(this)} 
+                     />
+  }
+
+  // render the user profile component
+  // renderUserProfile(){
+  //   return <UserProfile user={this.state.userInfo} 
+  //                       handleRegister={this.handleRegister.bind(this)} 
+  //                       renderSavedJob={this.renderSavedJob.bind(this)}
+  //                       setUserProfile={this.setUserProfile.bind(this)}
+  //                       setSelectedJob={this.setSelectedJob.bind(this)}
+  //                       />
   // }
 
+  // claer the state for log out
+  // handleLogout() {
+  //   this.setState({
+  //     username: '',
+  //     userInfo: undefined,
+  //     showProfile: false,
+  //     userForm: false
+  //   })
+  // }
+  
+  //This function will render the log-in form it the login is true
+  // renderLoginForm() {
+  //   return (
+  //     <div className="user-form">
+  //       <form className="show-form" onSubmit={this.handleSubmit.bind(this)}>
+  //         <div className="close-modal" onClick={ () => this.setLoginForm() }>x</div>
+  //         <label>Username: </label>
+  //         <input type="text" placeholder="Enter username" onChange={this.handleChange.bind(this)} />
+  //         <button>Login</button>
+  //       </form>
+  //     </div>
+  //   )
+  // }
+  
   render() {
     return (
       <div>
@@ -47,6 +204,10 @@ class App extends Component {
           navsRef={["Home", "About", "Activity", "Trip", "Contact" , "Login"]}
         />
       <About/>
+      <UserForm userInfo={this.state.userInfo} 
+                     handleFormSubmit={this.handleFormSubmit.bind(this)} 
+                     handleRegister={this.handleRegister.bind(this)} 
+                     />
 
       </div>
     );
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -11,7 +11,7 @@ class UserForm extends Component {
       password: props.userInfo ? props.userInfo.password : '',
       phone: props.userInfo ? props.userInfo.phone : '',
       gender: props.userInfo ? props.userInfo.gender : '',
-      location: props.userInfo ? props.userInfo.location: '',
+      location: props.userInfo ? props.userInfo.location : '',
       id: props.userInfo ? props.userInfo.id : null
     }
   }
@@ -28,75 +28,63 @@ class UserForm extends Component {
   //Handle Register and edit submission
   handleSubmit(event) {
     event.preventDefault();
-    this.props.handleFormSubmit(this.state) 
+    this.props.handleFormSubmit(this.state)
   }
-  // <div className="user-form">
-  //       <form className="show-form" onSubmit={this.handleSubmit.bind(this)}>
-  //         <div className="close-modal" onClick={() => this.props.handleRegister()}>x</div>
-  //         <label>username:</label><input type="text" value={this.state.username} name="username" onChange={this.handleChange.bind(this)} /><br />
-  //         <label>firstname:</label><input type="text" value={this.state.firstname} name="firstname" onChange={this.handleChange.bind(this)} /><br />
-  //         <label>lastname:</label><input type="text" value={this.state.lastname} name="lastname" onChange={this.handleChange.bind(this)} /><br />
-  //         <label>email:</label><input type="text" value={this.state.email} name="email" onChange={this.handleChange.bind(this)} /><br />
-  //         <label>phone:</label><input type="text" value={this.state.phone} name="phone" onChange={this.handleChange.bind(this)} /><br />
-  //         <button>submit</button>
-  //       </form>
-  //     </div>
-  render()
-   {
-    return( 
-    <div className="form">
-<form>
-  <div className="form-row">
-    <div className="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
-       </div>
-      <div className="form-group col-md-6">
-        <label for="inputPassword4">Password</label>
-        <input type="password" className="form-control" id="inputPassword4" placeholder="Password"/>
-       </div>
-      </div>
-      <div className="form-group">
-        <label for="inputAddress">Address</label>
-        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
-       </div>
-        <div className="form-group">
-          <label for="inputAddress2">Address 2</label>
-          <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-         </div>
+
+  render() {
+    return (
+      <div className="form">
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <div className="form-row">
             <div className="form-group col-md-6">
-              <label for="inputCity">City</label>
-              <input type="text" class="form-control" id="inputCity"/>
+              <label for="inputEmail4">Email</label>
+              <input type="email" class="form-control" id="inputEmail4" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange.bind(this)} />
             </div>
-              <div className="form-group col-md-4">
-                <label for="inputState">State</label>
-                <select id="inputState" className="form-control">
-                  <option selected>Choose...</option>
-                  <option>...</option>
-                </select>
-              </div>
-              <div className="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" className="form-control" id="inputZip"/>
-               </div>
-              </div>
-              <div className="form-group">
-                <div className="form-check">
-                  <input className="form-check-input" type="checkbox" id="gridCheck"/>
-                    <label className="form-check-label" for="gridCheck">
-                      Check me out
-                      </label>
-                     </div>
-                      </div>
-                <button type="submit" className="btn btn-primary">Sign in</button>
-</form>
+            <div className="form-group col-md-6">
+              <label for="inputEmail4">Username</label>
+              <input type="text" class="form-control" id="username" placeholder="username" value={this.state.username} name="username" onChange={this.handleChange.bind(this)} />
+            </div>
+            <div className="form-group col-md-6">
+              <label for="inputEmail4">Phone</label>
+              <input type="text" class="form-control" id="phone" placeholder="966 55555 5555" value={this.state.phone} name="phone" onChange={this.handleChange.bind(this)} />
+            </div>
+            <div className="form-group col-md-6">
+              <label for="inputPassword4">Password</label>
+              <input type="password" className="form-control" id="inputPassword4" placeholder="Password" value={this.state.password} name="password"  onChange={this.handleChange.bind(this)} />
+            </div>
+          </div>
 
-    </div>
-                    
-                 )
-                }
-                }
-                
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label for="firstname">First Name</label>
+              <input type="text" class="form-control" id="firstname" value={this.state.firstname} name="firstname" onChange={this.handleChange.bind(this)} />
+            </div>
+            <div className="form-group col-md-6">
+              <label for="lastname">Last Name</label>
+              <input type="text" class="form-control" id="lastname" value={this.state.lastname} name="lastname" onChange={this.handleChange.bind(this)} />
+            </div>
+            <div className="form-group col-md-4">
+              <label for="gender">Gender</label>
+              <select id="gender" className="form-control">
+                <option selected>Choose...</option>
+                <option>Male</option>
+                <option>Female</option>
+              </select>
+            </div>
+            <div className="form-group col-md-2">
+              <label for="location">Location</label>
+              <input type="text" className="form-control" id="location" value={this.state.location} name="location" onChange={this.handleChange.bind(this)}/>
+            </div>
+          </div>
+
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+
+      </div>
+
+    )
+  }
+}
+
 export default UserForm;
 
